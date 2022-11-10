6495e288643545a51a5e13fb04b3e78453ee0596
jackydnb <jackydnb@gmail.com>
2022-11-10 17:36:32 +0300

    baeldung-update

--- src/main/java/pageobject/uiobjects/example/site/pages/HomePage.java
+++ src/main/java/pageobject/uiobjects/example/site/pages/HomePage.java
@@ -4,9 +4,9 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
 import com.epam.jdi.light.elements.pageobjects.annotations.Title;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
-import com.epam.jdi.light.elements.pageobjects.annotations.simple.UI;
-import com.epam.jdi.light.elements.pageobjects.annotations.simple.WithText;
-import com.epam.jdi.light.ui.html.common.Link;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.WithText;
+import com.epam.jdi.light.ui.html.elements.common.Link;
 
 public class HomePage extends WebPage {
     @UI(".menu-about a") public static Link menuAbout;
